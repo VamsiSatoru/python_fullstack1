ef236e9d3ef84e9f0a8f3960655a8628107898bc
VaMsI KrIsHnA <vamsikrishnar349@gmail.com>
2026-07-28 10:39:23 +0000

    js

---
+++ js/test.js
@@ -0,0 +1,35 @@
+console.log("Hello World")
+let student_name = "Krishna"
+console.log("Student name is: "+ student_name)
+let student_age = 22
+let course_name = "Python Full Stack"
+console.log("Student age is: "+ student_age, "course name is: "+ course_name)
+let fee = 10000
+let discount = 10
+let discounted_fee = fee - (fee * discount / 100)
+console.log("Discounted fee is: "+ discounted_fee)
+
+let age=18
+if(age>=18){
+    console.log("Eligible for admission")
+}
+else{
+    console.log("Not eligible for admission")
+}
+for(let i=1; i<=5; i++){
+    console.log("Iterationj number: " + i)
+}
+const pi = 3.14
+console.log("Value of pi is: " + pi)
+let student ={
+    name: "Sriram",
+    age: 21,
+    course: "Python Full Stack",
+    fee: 10000
+}
+console.log("Student details: ", student)
+function greetStudent(name){
+    console.log("Hello" + Krishna 
+        + ", welcome to NRIIT Learning Management System")
+}
+greetStudent(student_name)
